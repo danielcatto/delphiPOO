5d438328cac1b0d8bd9d8578e6c2404939fee11f
danielcatto <danielcatto@hotmail.com>
2025-02-24 23:52:13 -0300

    Poliformismo classe abstract

---
+++ Polimorfismo-Abstract/Poliformismo.Abstract.dpr
@@ -0,0 +1,16 @@
+program Poliformismo.Abstract;
+
+uses
+  Vcl.Forms,
+  View.Principal in 'src\View.Principal.pas' {Form1},
+  Classe.Pessoa in 'src\Classe.Pessoa.pas',
+  Classe.Cliente in 'src\Classe.Cliente.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
